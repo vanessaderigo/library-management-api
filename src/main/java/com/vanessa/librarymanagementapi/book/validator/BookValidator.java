50ba21f2f4037cf0592d0401908e56dee732ac86
Vanessa <vderigoesteves@gmail.com>
2025-08-03 18:34:44 -0300

    feat(book): add validation requiring price for books published from 2020

--- src/main/java/com/vanessa/librarymanagementapi/book/validator/BookValidator.java
+++ src/main/java/com/vanessa/librarymanagementapi/book/validator/BookValidator.java
@@ -3,6 +3,7 @@ package com.vanessa.librarymanagementapi.book.validator;
 import com.vanessa.librarymanagementapi.book.model.Book;
 import com.vanessa.librarymanagementapi.book.repository.BookRepository;
 import com.vanessa.librarymanagementapi.exceptions.DuplicateEntryException;
+import com.vanessa.librarymanagementapi.exceptions.InvalidFieldException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -11,11 +12,16 @@ import java.util.List;
 @Component
 @RequiredArgsConstructor
 public class BookValidator {
+    private static final int YEAR = 2020;
+
     private final BookRepository repository;
 
     public void validate(Book book){
         if (existsByIsbn(book)){
             throw new DuplicateEntryException("Isbn already registered.");
+        }
+        if (isRequiredPriceNull(book)){
+            throw new InvalidFieldException("price", "For books published in 2020 or later, the price is required.");
         }
     }
 
@@ -27,5 +33,10 @@ public class BookValidator {
         }
 
         return books.stream().anyMatch(b -> !b.getId().equals(book.getId()));
+    }
+
+    private boolean isRequiredPriceNull(Book book){
+        return book.getPrice() == null &&
+                book.getPublicationDate().getYear() >= YEAR;
     }
 }
