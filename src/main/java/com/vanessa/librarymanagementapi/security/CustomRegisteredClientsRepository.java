434b4b01c9c21c55bd1690c0e4a20cc999758e31
Vanessa <vderigoesteves@gmail.com>
2025-08-06 19:20:10 -0300

    feat: add AuthorizationServerConfiguration with token and client settings

--- src/main/java/com/vanessa/librarymanagementapi/security/CustomRegisteredClientsRepository.java
+++ src/main/java/com/vanessa/librarymanagementapi/security/CustomRegisteredClientsRepository.java
@@ -6,12 +6,16 @@ import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class CustomRegisteredClientsRepository implements RegisteredClientRepository {
     private final ClientService service;
+    private final TokenSettings tokenSettings;
+    private final ClientSettings clientSettings;
 
     @Override
     public void save(RegisteredClient registeredClient) {}
@@ -36,6 +40,8 @@ public class CustomRegisteredClientsRepository implements RegisteredClientReposi
                 .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
                 .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
                 .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                .tokenSettings(tokenSettings)
+                .clientSettings(clientSettings)
                 .build();
     }
 }
