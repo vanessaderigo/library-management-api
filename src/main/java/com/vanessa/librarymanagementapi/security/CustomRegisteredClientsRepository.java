6bba2864ba7f5ec97c50b3feb90976ab74c115d3
Vanessa <vderigoesteves@gmail.com>
2025-08-06 19:08:43 -0300

    feat: create custom RegisteredClientsRepository

---
+++ src/main/java/com/vanessa/librarymanagementapi/security/CustomRegisteredClientsRepository.java
@@ -0,0 +1,41 @@
+package com.vanessa.librarymanagementapi.security;
+
+import com.vanessa.librarymanagementapi.client.service.ClientService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CustomRegisteredClientsRepository implements RegisteredClientRepository {
+    private final ClientService service;
+
+    @Override
+    public void save(RegisteredClient registeredClient) {}
+
+    @Override
+    public RegisteredClient findById(String id) {
+        return null;
+    }
+
+    @Override
+    public RegisteredClient findByClientId(String clientId) {
+        var client = service.getByClientId(clientId);
+
+        if (client == null) return null;
+
+        return RegisteredClient
+                .withId(client.getId().toString())
+                .clientId(client.getClientId())
+                .clientSecret(client.getClientSecret())
+                .redirectUri(client.getRedirectURI())
+                .scope(client.getScope())
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                .build();
+    }
+}
