a9fc2feea3d8f997edcf6691f2d7b8c732b8421a
Vanessa <vderigoesteves@gmail.com>
2025-08-06 22:21:05 -0300

    feat: add view endpoint to display authorization code

--- src/main/java/com/vanessa/librarymanagementapi/configuration/web/LoginViewController.java
+++ src/main/java/com/vanessa/librarymanagementapi/configuration/web/LoginViewController.java
@@ -4,6 +4,7 @@ import com.vanessa.librarymanagementapi.security.CustomAuthentication;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -18,5 +19,11 @@ public class LoginViewController {
     public String homePage(Authentication authentication){
         if (authentication instanceof CustomAuthentication customAuth) System.out.println(customAuth.getUser());
         return "Hello, " + authentication.getName() + "!";
+    }
+
+    @GetMapping("/authorized")
+    @ResponseBody
+    public String getAuthorizationCode(@RequestParam("code") String code){
+        return "Authorization code: " + code;
     }
 }
