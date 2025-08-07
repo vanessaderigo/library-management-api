1e24f3936a0a303e793b57f542af903bab1dcd4e
Vanessa <vderigoesteves@gmail.com>
2025-08-07 00:43:53 -0300

    feat(api): document UserController with Swagger

--- src/main/java/com/vanessa/librarymanagementapi/user/controller/UserController.java
+++ src/main/java/com/vanessa/librarymanagementapi/user/controller/UserController.java
@@ -5,6 +5,10 @@ import com.vanessa.librarymanagementapi.user.dto.UserDTO;
 import com.vanessa.librarymanagementapi.user.mapper.UserMapper;
 import com.vanessa.librarymanagementapi.user.model.User;
 import com.vanessa.librarymanagementapi.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -14,12 +18,15 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/users")
+@Tag(name = "Users")
 @RequiredArgsConstructor
 public class UserController implements GenericController {
     private final UserService service;
     private final UserMapper mapper;
 
     @PostMapping
+    @Operation(summary = "Save", description = "Register a user.")
+    @ApiResponse(responseCode = "201", description = "Registered successfully.")
     public ResponseEntity<Object> save(@RequestBody UserDTO dto){
         User user = mapper.toEntity(dto);
         service.save(user);
@@ -29,6 +36,10 @@ public class UserController implements GenericController {
 
     @PutMapping("/{id}")
     @PreAuthorize("hasAnyRole('OPERATOR', 'MANAGER')")
+    @Operation(summary = "Update", description = "Update an existing user.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "Successfully updated."),
+            @ApiResponse(responseCode = "404", description = "User not found.")})
     public ResponseEntity<Object> update(@PathVariable String id, @RequestBody UserDTO dto){
         var userId = UUID.fromString(id);
         return service.getById(userId).map(user -> {
@@ -44,6 +55,10 @@ public class UserController implements GenericController {
 
     @DeleteMapping("/{id}")
     @PreAuthorize("hasRole('MANAGER')")
+    @Operation(summary = "Delete", description = "Deletes user by id.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "Successfully deleted."),
+            @ApiResponse(responseCode = "404", description = "User not found.")})
     public ResponseEntity<Object> delete(@PathVariable String id){
         var userId = UUID.fromString(id);
         return service.getById(userId).map(user -> {
--- src/main/java/com/vanessa/librarymanagementapi/user/dto/UserDTO.java
+++ src/main/java/com/vanessa/librarymanagementapi/user/dto/UserDTO.java
@@ -1,6 +1,9 @@
 package com.vanessa.librarymanagementapi.user.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.util.List;
 
+@Schema(name = "User")
 public record UserDTO(String login, String email, String password, List<String> roles){
 }
